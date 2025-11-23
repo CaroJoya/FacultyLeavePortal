58f496929ed692254922dd0c1055e6ed4ac3b80a
CaroJoya <s75362918@gmail.com>
2025-11-24 01:42:01 +0530

    Update main.py  #deploything

--- Flask college work/main.py
+++ Flask college work/main.py
@@ -1469,4 +1469,5 @@ def admin_reset_faculty_password(faculty_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
